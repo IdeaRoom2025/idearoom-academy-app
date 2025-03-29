4ce2e21488cdd65b1ccfce06f54045c5fd642742
Saba <sgachechiladze881@gmail.com>
2025-03-29 04:48:11 +0400

    final

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function notFound() {
   return (
-    <section className="container text-center items-center justify-center mt-[200px]">
+    <section className="container max-md:max-w-[80%] text-center items-center justify-center mt-[200px]">
       <h2 className="text-[36px] lg:text-[48px] xl:text-[64px]  text-primary-500 font-bold">
         404
       </h2>
