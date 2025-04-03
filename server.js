dc92c2d44ccf9ae1a0e22e9fa7d30d404176542a
Kukhianidze <revazi.kukhianidze@majorel.com>
2025-04-03 15:34:28 +0400

    changed server.js

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
+    const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
   }).listen(port)
  
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
 })
